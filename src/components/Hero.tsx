ea19cce02982a5632f01110c777c55a2ea52a2aa
Opeyemi Omotunde <opeyemiomotunde56@gmail.com>
2025-05-20 09:47:23 +0100

    "Merged the two available branch"

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 // import React from 'react'
-
+import { MdDocumentScanner } from "react-icons/md";
 import brain2 from "../assets/Images/brain2.png";
 import "../index.css";
 import file from "../assets/Images/file.png";
@@ -68,9 +68,18 @@ const Hero = () => {
                   <h1 className="text-sm font-quicksand">AI Detector</h1>
                 </div>
               </div>
-              <div className="flex gap-2 items-center">
-                <img className="w-6" src={file} alt="" />
-                <h1>Plagiarism Checker</h1>
+              <div className="flex gap-2 items-center ml-10 text-nowrap ">
+                <img
+                  className="font-semibold text-sm font-quicksand w-6"
+                  src={file}
+                  alt=""
+                />
+                <h1 className="block md:hidden text-sm font-quicksand">
+                  Plagiarism
+                </h1>
+                <h1 className="hidden md:block text-sm font-quicksand">
+                  Plagiarism Checker
+                </h1>
               </div>
             </div>
             <fieldset className="border border-gray-300 p-1 w-[250px] rounded-2xl">
@@ -93,31 +102,56 @@ const Hero = () => {
             onChange={(e) => setInputText(e.target.value)}
           />
 
-          <div className="flex flex-wrap items-center justify-between mt-14 gap-2 w-full">
-            <div className="flex items-center gap-4">
-              <p className="text-sm font-semibold">Word Count: 0</p>
-
-              <div className="h-6 w-px bg-gray-300"></div>
-
-              <div className="flex items-center gap-2">
-                <img className="w-5" src={coins} alt="coins" />
-                <h2 className="text-sm font-semibold">2 credits left</h2>
-                <button className="border-2 border-gray-200 rounded-xl px-4 py-1 text-sm font-semibold cursor-default">
-                  Buy more credits
-                </button>
-              </div>
-            </div>
-            <div className="flex items-center gap-2">
-              <div className="flex gap-2 text-black">
-                <img className="w-4 h-5" src={arrowup} alt="" />
-                <p className="hidden md:block">Upload</p>
+          <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between mt-6 gap-4 w-full">
+            <div className="flex items-center gap-4 flex-wrap">
+              <p className="font-quicksand text-sm text-slate-400 font-semibold">
+                Word Count: {charCount}
+              </p>
+              <div className="hidden md:block  h-6 w-px bg-gray-300"></div>
+              <div className="flex items-center gap-2 flex-wrap">
+                <img className="w-5 md:mr-1 mr-2" src={coins} alt="coins" />
+                <h2 className="hidden md:block text-sm font-semibold">
+                  2 credits left
+                </h2>
+                <h2 className="block md:hidden text-sm font-semibold">2</h2>
               </div>
 
-              <button className="flex md:flex-row flex-col items-center justify-center font-nunito bg-black text-white hover:text-slate-500 hover:bg-black rounded-full md:rounded-md text-sm font-semibold cursor-pointer text-center w-9 h-9 md:w-auto md:px-6 md:py-2 md:ml-4 ml-2">
-                <MdDocumentScanner className="md:mt-1 md:mr-2 block md:hidden" />
-                <p className="hidden md:block">Scan Content</p>
+              <button className="border-2 border-gray-200 rounded-xl px-4 py-1 text-sm font-semibold font-quicksand cursor-default md:ml-4 ml-5">
+                <p className="hidden md:block"> Buy more credits</p>
+                <p className="block md:hidden">Buy</p>
               </button>
             </div>
+          </div>
+          <div className="flex items-center gap-2">
+            <label
+              htmlFor="fileUpload"
+              title="Upload a .txt file"
+              className="flex items-center gap-2 cursor-pointer"
+            >
+              <img className="w-4 h-5" src={arrowup} alt="Upload icon" />
+              <p className="hidden sm:block">Upload</p>
+            </label>
+            <input
+              id="fileUpload"
+              type="file"
+              accept=".txt"
+              onChange={handleFileChange}
+              className="hidden"
+            />
+            <button
+              onClick={handleScan}
+              className={`flex md:flex-row flex-col items-center justify-center font-nunito bg-black text-white hover:text-slate-500 hover:bg-black rounded-full md:rounded-md text-sm font-semibold cursor-pointer text-center w-9 h-9 md:w-auto md:px-6 md:py-2 md:ml-4 ml-2transition-all duration-300 ${
+                loading
+                  ? "bg-gray-500 text-black cursor-pointer"
+                  : "bg-black text-white hover:text-slate-500 hover:bg-black cursor-pointer"
+              }`}
+            >
+              <MdDocumentScanner className="md:mt-1 md:mr-2 block md:hidden" />
+              <p className="hidden md:block">
+                {loading ? "Scanning" : "Scan Content"}
+              </p>
+              <p className="block md:hidden">Scan</p>
+            </button>
           </div>
         </div>
       </div>
